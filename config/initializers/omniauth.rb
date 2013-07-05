e01a152e0473d1e1705595bb0da5bb7328ce148b
Joel Walters <jtwalters@gmail.com>
2013-07-05 11:18:42 -0700

    Add Facebook authentication option

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,4 +4,5 @@ OmniAuth.config.test_mode = Rails.env.test?
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
   provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET'], {:access_type => 'online', :approval_prompt => ''}
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
 end
